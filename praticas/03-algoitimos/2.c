816c963a186fb7105f8829198e617d4b260140e8
Maxine Klein <87439325+MaxKGS@users.noreply.github.com>
2026-04-21 14:30:29 -0300

    Update 2.c

--- praticas/03-algoitimos/2.c
+++ praticas/03-algoitimos/2.c
@@ -1,79 +1,61 @@
-/*
-Insira o valor do produto e o codigo de desconto. O codigo tem que ser um char.
-Imprima na tela o valor final do produto.
-Caso o pre�o apos o desconto seja maior que 500, oferecer parcelar.
-Depois, imprimir o pre�o parcelado.
-
-Codigo feito por: Maxine Klein
+/* =========================================================================
+ * Autor        : Maxine Klein
+ * Data de Criação : 20/03/2026
+ * Descrição    : Calcula o valor final de um produto com desconto baseado
+ *                na cor da etiqueta e oferece parcelamento sem juros.
+ * =========================================================================
 */
 
 #include <stdio.h>
-#include <math.h>
+#include <ctype.h>
 
-int main()
-{
-    char codCor;
-    float valorProd, desconto, valorParc;
-    int quantParcelas;
+int main() {
+    double valorProduto, desconto, valorFinal;
+    char corEtiqueta, opcaoParcela;
+    int numParcelas;
 
-    printf("Insira o valor do produto:\n");
-    scanf("%f", &valorProd);
+    printf("Entre o valor do produto: ");
+    scanf("%lf", &valorProduto);
 
-    printf("Insira o codigo de desconto, representado por apenas seu primeiro caractere:\n");
-    scanf(" %c", &codCor);
+    printf("Entre o codigo de cor da etiqueta: ");
+    scanf(" %c", &corEtiqueta);
 
-    switch(codCor)  //escolhe qual desconto deve ser aplicado em cima do caractere escolhido
-    {
-    case 'a':
-    case 'A':
-        printf("O codigo inserido possui 10 porcento de desconto\n");
-        valorProd -= valorProd * 0.10;
-        break;
-    case 'v':
-    case 'V':
-        printf("O codigo inserido possui 20 porcento de desconto\n");
-        valorProd -= valorProd * 0.20;
-        break;
-    case 'b':
-    case 'B':
-        printf("O codigo inserido possui 30 porcento de desconto\n");
-        valorProd -= valorProd * 0.30;
-        break;
-    case 'p':
-    case 'P':
-        printf("O codigo inserido possui 40 porcento de desconto\n");
-        valorProd -= valorProd * 0.40;
-        break;
-    default:
-        printf("O codigo inserido nao possui desconto\n");
-        break;
+    corEtiqueta = toupper(corEtiqueta);
+
+    switch (corEtiqueta) {
+        case 'A':
+            desconto = 0.10;
+            break;
+        case 'V':
+            desconto = 0.20;
+            break;
+        case 'B':
+            desconto = 0.30;
+            break;
+        case 'P':
+            desconto = 0.40;
+            break;
+        default:
+            desconto = 0.0;
+            break;
     }
 
-    printf("O valor final do produto eh: %0.2f reais\n", valorProd);
-    printf("\n");
+    valorFinal = valorProduto * (1.0 - desconto);
 
-    if(valorProd >500.00)  //testa se valor final esta apto para descontos (precisa estar acima de 500 reais)
-    {
-        char parcelas;
-        printf("O seu produto custa mais de 500.00, entao voce pode parcelar!\n");
-        printf("Deseja parcelar? (S/N)\n");
-        scanf(" %c", &parcelas);
+    printf("O valor final do produto eh de %.2lf reais.\n", valorFinal);
 
-        if(parcelas == 's' || parcelas == 'S')  //testa se o usuario quer parcelar o produto
-        {
-            printf("Em quantas vezes quer parcelar?\n");
-            scanf("%d", &quantParcelas);
+    if (valorFinal > 500.00) {
+        printf("Voce deseja parcelar esse valor (S/N)? ");
+        scanf(" %c", &opcaoParcela);
+        opcaoParcela = toupper(opcaoParcela);
 
-            if(parcelas > 0)
-            {
-                valorParc = valorProd / (float)quantParcelas;
-                printf("O valor de cada parcela eh: %0.2f reais", valorParc);
-            }
-            else
-            {
-                printf("Numero de parcelas inseridas nao podem ser menores ou igual a zero");
-            }
+        if (opcaoParcela == 'S') {
+            printf("Em quantas parcelas? ");
+            scanf("%d", &numParcelas);
+            printf("O valor de cada parcela eh de %.2lf reais\n", valorFinal / numParcelas);
         }
     }
+
     return 0;
 }
+
